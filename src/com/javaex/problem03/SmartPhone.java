b77b5a5b91430afeca7da356ebf652efc5366673
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-09 20:45:37 +0900

    3

--- src/com/javaex/problem03/SmartPhone.java
+++ src/com/javaex/problem03/SmartPhone.java
@@ -3,14 +3,22 @@ package com.javaex.problem03;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
-        
-        //코드작성
+        if(str.equals("앱")) {
+        	this.playApp();
+        }else if(str.equals("음악")) {
+        	this.playMusic();
+        }else if(str.equals("통화")) {
+        	super.execute(str);
+        }
+       
         
     }
+
+	private void playApp() {
+    	System.out.println("앱실행");
+    }
  
-    //메소드작성
-    
-    //메소드작성
+  
     
     
     
