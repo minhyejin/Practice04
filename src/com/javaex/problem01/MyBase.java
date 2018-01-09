5a0e52ca358d70dd67c0a48fa914f0e7871ad9f9
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-09 20:12:54 +0900

    1

--- src/com/javaex/problem01/MyBase.java
+++ src/com/javaex/problem01/MyBase.java
@@ -2,6 +2,17 @@ package com.javaex.problem01;
 
 public class MyBase extends Base{
 
-    //코드작성
+	@Override
+	public void service(String state) {
+	if(state.equals("낮")) {
+		this.day();
+	}else if(state.equals("밤")) {
+		this.night();
+	}else {
+		System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+	}
+	}
+
+   
     
 }
