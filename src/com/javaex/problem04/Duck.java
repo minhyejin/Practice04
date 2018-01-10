c2f1e89255e118aac39a51c717cb2132d2709ea2
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-10 09:34:51 +0900

    1

--- src/com/javaex/problem04/Duck.java
+++ src/com/javaex/problem04/Duck.java
@@ -3,15 +3,15 @@ package com.javaex.problem04;
 public class Duck extends Bird {
 
     public void sing() {
-    
+    	System.out.println("오리 ("+getName()+") 소리내어 웁니다.");
     }
 
     public void fly() {
-    
+    	System.out.println("오리 ("+getName()+") 가 날지않습니다");
     }
     
     public void showName() {
-    
+    	System.out.println("오리 ("+getName()+") 의 이름은 꽥꽥이 입니다.");
     }
 
 }
--- src/com/javaex/problem04/Sparrow.java
+++ src/com/javaex/problem04/Sparrow.java
@@ -2,6 +2,22 @@ package com.javaex.problem04;
 
 public class Sparrow extends Bird {
 
+	@Override
+	public void sing() {
+		System.out.println("참새("+getName()+")소리내어 웁니다.");
+	}
 
+	@Override
+	public void fly() {
+		System.out.println("참새("+getName()+")가 날아다닙니다");
+	}
+
+	@Override
+	public void showName() {
+		System.out.println("참새("+getName()+")의 이름은 짹짹이 입니다.");
+	}
+
+
+	
 
 }
